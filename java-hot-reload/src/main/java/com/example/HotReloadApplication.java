17422d46536a3c7b769d045f415a57dde35ec8a1
kvrs <43546720+kvrs@users.noreply.github.com>
2026-01-28 18:32:27 +0530

    Enable configuration properties for DemoConfig

--- java-hot-reload/src/main/java/com/example/HotReloadApplication.java
+++ java-hot-reload/src/main/java/com/example/HotReloadApplication.java
@@ -2,8 +2,10 @@ package com.example.hotreload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DemoConfig.class)
 public class HotReloadApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotReloadApplication.class, args);
