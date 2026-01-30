23011cebcec95780a4f61a04e2e3e703a7aba5dd
kvrs <43546720+kvrs@users.noreply.github.com>
2026-01-30 15:59:56 +0530

    Remove unused imports from HotReloadApplication

--- java-hot-reload/src/main/java/com/example/HotReloadApplication.java
+++ java-hot-reload/src/main/java/com/example/HotReloadApplication.java
@@ -1,9 +1,3 @@
-package com.example.hotreload;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 @SpringBootApplication
 @EnableConfigurationProperties(DemoConfig.class)
 public class HotReloadApplication {
