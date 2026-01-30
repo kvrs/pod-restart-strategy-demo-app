ae5c01f7912bbec08d676f57b88d3d71f47041db
kvrs <43546720+kvrs@users.noreply.github.com>
2026-01-30 16:14:35 +0530

    Update HotReloadApplication.java

--- java-hot-reload/src/main/java/com/example/hotreload/HotReloadApplication.java
+++ java-hot-reload/src/main/java/com/example/hotreload/HotReloadApplication.java
@@ -1,3 +1,9 @@
+package com.example.hotreload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 @SpringBootApplication
 @EnableConfigurationProperties(DemoConfig.class)
 public class HotReloadApplication {
