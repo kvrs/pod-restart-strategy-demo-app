072a22a298201cd55e52e6498558ca7de0d865bc
kvrs <43546720+kvrs@users.noreply.github.com>
2026-01-30 16:15:12 +0530

    Delete java-hot-reload/src/main/java/com/example/HotReloadApplication.java

--- java-hot-reload/src/main/java/com/example/HotReloadApplication.java
+++
@@ -1,7 +0,0 @@
-@SpringBootApplication
-@EnableConfigurationProperties(DemoConfig.class)
-public class HotReloadApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HotReloadApplication.class, args);
-    }
-}
